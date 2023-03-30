d0dfe2398251bdff24bf042a89d5900dddf155f2
henry-shih4 <100496651+henry-shih4@users.noreply.github.com>
2023-03-30 01:36:50 -0500

    added fetching evolution chain

--- src/Pages/PokemonProfile/Pokemon.jsx
+++ src/Pages/PokemonProfile/Pokemon.jsx
@@ -1,12 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams, useLocation } from "react-router-dom";
 import axios from "axios";
 import Card from "./Card";
+
+import { PokemonContext } from "../../components/PokemonContext";
 
 export default function Pokemon() {
   const { name } = useParams();
   const [pokeData, setPokeData] = useState([]);
   const { state } = useLocation();
+  const [speciesData, setSpeciesData] = useState();
+  const [evolutionData, setEvolutionData] = useState();
+  const [firstEvo, setFirstEvo] = useState(null);
+  const [secondEvo, setSecondEvo] = useState(null);
+  const [evolutions, setEvolutions] = useState(null);
+  const { pokeList, loading } = useContext(PokemonContext);
 
   const fetchPokemon = async () => {
     console.log("making new fetch request");
@@ -14,13 +22,57 @@ export default function Pokemon() {
     setPokeData(data.data);
   };
 
+  const fetchSpecies = async () => {
+    const data = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon-species/${name}/`
+    );
+    setSpeciesData(data.data);
+
+    const evolutionChain = await axios.get(data.data.evolution_chain.url);
+    setEvolutionData(evolutionChain.data);
+  };
+
   useEffect(() => {
+    fetchSpecies();
     if (state) {
       setPokeData(state.item);
     } else {
       fetchPokemon();
     }
   }, []);
+
+  function filterEvolution(one, two) {
+    console.log("filtering evo");
+    if (one || two) {
+      const evolve = pokeList.filter((item) => {
+        return item.name == one || item.name == two;
+      });
+      setEvolutions(evolve);
+    }
+  }
+
+  useEffect(() => {
+    if (evolutionData) {
+      if (evolutionData.chain.evolves_to[0]) {
+        setFirstEvo(evolutionData.chain.evolves_to[0].species.name);
+      }
+      if (evolutionData.chain.evolves_to[0].evolves_to[0]) {
+        setSecondEvo(
+          evolutionData.chain.evolves_to[0].evolves_to[0].species.name
+        );
+      }
+    }
+  }, [evolutionData]);
+
+  useEffect(() => {
+    if (firstEvo || secondEvo) {
+      filterEvolution(firstEvo, secondEvo);
+    }
+  }, [firstEvo, secondEvo]);
+
+  useEffect(() => {
+    console.log(firstEvo, secondEvo);
+  });
 
   return (
     <>
@@ -38,6 +90,19 @@ export default function Pokemon() {
           </>
         ) : null}
       </div>
+
+      {evolutions ? (
+        <>
+          <div>
+            {evolutions[0] ? (
+              <img src={evolutions[0].sprites.front_default} />
+            ) : null}
+          </div>
+          {evolutions[1] ? (
+            <img src={evolutions[1].sprites.front_default} />
+          ) : null}
+        </>
+      ) : null}
     </>
   );
 }
