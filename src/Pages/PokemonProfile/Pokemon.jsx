62798cf43e25ee12c16416ef33a4bc5c280bc373
henry-shih4 <100496651+henry-shih4@users.noreply.github.com>
2023-04-01 01:08:31 -0500

    working on getting evolution data

--- src/Pages/PokemonProfile/Pokemon.jsx
+++ src/Pages/PokemonProfile/Pokemon.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import { useParams, useLocation } from "react-router-dom";
 import axios from "axios";
 import Card from "./Card";
-
+import Loading from "../../components/Loading";
 import { PokemonContext } from "../../components/PokemonContext";
 
 export default function Pokemon() {
@@ -11,8 +11,9 @@ export default function Pokemon() {
   const { state } = useLocation();
   const [speciesData, setSpeciesData] = useState();
   const [evolutionData, setEvolutionData] = useState();
-  const [firstEvo, setFirstEvo] = useState(null);
-  const [secondEvo, setSecondEvo] = useState(null);
+  const [baseEvo, setBaseEvo] = useState("");
+  const [firstEvo, setFirstEvo] = useState("");
+  const [secondEvo, setSecondEvo] = useState("");
   const [evolutions, setEvolutions] = useState(null);
   const { pokeList, loading } = useContext(PokemonContext);
 
@@ -29,6 +30,7 @@ export default function Pokemon() {
     setSpeciesData(data.data);
 
     const evolutionChain = await axios.get(data.data.evolution_chain.url);
+    console.log(evolutionChain);
     setEvolutionData(evolutionChain.data);
   };
 
@@ -41,11 +43,11 @@ export default function Pokemon() {
     }
   }, []);
 
-  function filterEvolution(one, two) {
+  function filterEvolution(base, one, two) {
     console.log("filtering evo");
     if (one || two) {
       const evolve = pokeList.filter((item) => {
-        return item.name == one || item.name == two;
+        return item.name == base || item.name == one || item.name == two;
       });
       setEvolutions(evolve);
     }
@@ -53,25 +55,77 @@ export default function Pokemon() {
 
   useEffect(() => {
     if (evolutionData) {
-      if (evolutionData.chain.evolves_to[0]) {
-        setFirstEvo(evolutionData.chain.evolves_to[0].species.name);
+      setBaseEvo(evolutionData.chain.species.name);
+
+      if (
+        evolutionData.chain.evolves_to[0] &&
+        evolutionData.chain.evolves_to[0].evolution_details[0].trigger.name !==
+          "use-item"
+      ) {
+        setFirstEvo({
+          evolution: evolutionData.chain.evolves_to[0].species.name,
+          trigger:
+            evolutionData.chain.evolves_to[0].evolution_details[0].trigger.name,
+          level:
+            evolutionData.chain.evolves_to[0].evolution_details[0].min_level,
+        });
       }
-      if (evolutionData.chain.evolves_to[0].evolves_to[0]) {
-        setSecondEvo(
-          evolutionData.chain.evolves_to[0].evolves_to[0].species.name
-        );
+      if (
+        evolutionData.chain.evolves_to[0].evolves_to[0] &&
+        evolutionData.chain.evolves_to[0].evolves_to[0].evolution_details[0]
+          .trigger.name !== "use-item"
+      ) {
+        setSecondEvo({
+          evolution:
+            evolutionData.chain.evolves_to[0].evolves_to[0].species.name,
+          trigger:
+            evolutionData.chain.evolves_to[0].evolves_to[0].evolution_details[0]
+              .trigger.name,
+          level:
+            evolutionData.chain.evolves_to[0].evolves_to[0].evolution_details[0]
+              .min_level,
+        });
+      } else if (
+        evolutionData.chain.evolves_to[0] &&
+        evolutionData.chain.evolves_to[0].evolution_details[0].trigger.name ===
+          "use-item"
+      ) {
+        setFirstEvo({
+          evolution: evolutionData.chain.evolves_to[0].species.name,
+          trigger:
+            evolutionData.chain.evolves_to[0].evolution_details[0].trigger.name,
+          item: evolutionData.chain.evolves_to[0].evolution_details[0].item
+            .name,
+        });
+      } else if (
+        evolutionData.chain.evolves_to[0].evolves_to[0] &&
+        evolutionData.chain.evolves_to[0].evolves_to[0].evolution_details[0]
+          .trigger.name === "use-item"
+      ) {
+        setSecondEvo({
+          evolution:
+            evolutionData.chain.evolves_to[0].evolves_to[0].species.name,
+          trigger:
+            evolutionData.chain.evolves_to[0].evolves_to[0].evolution_details[0]
+              .trigger.name,
+          item: evolutionData.chain.evolves_to[0].evolves_to[0]
+            .evolution_details[0].item.name,
+        });
       }
     }
   }, [evolutionData]);
 
   useEffect(() => {
     if (firstEvo || secondEvo) {
-      filterEvolution(firstEvo, secondEvo);
+      filterEvolution(baseEvo, firstEvo.evolution, secondEvo.evolution);
     }
-  }, [firstEvo, secondEvo]);
+  }, [firstEvo, secondEvo, evolutionData]);
 
   useEffect(() => {
-    console.log(firstEvo, secondEvo);
+    console.log(evolutionData);
+    console.log(evolutions);
+    console.log(baseEvo, firstEvo, secondEvo);
+    console.log(speciesData);
   });
 
   return (
@@ -95,14 +149,36 @@ export default function Pokemon() {
         <>
           <div>
             {evolutions[0] ? (
-              <img src={evolutions[0].sprites.front_default} />
+              <>
+                <img src={evolutions[0].sprites.front_default} />
+              </>
             ) : null}
           </div>
-          {evolutions[1] ? (
-            <img src={evolutions[1].sprites.front_default} />
-          ) : null}
+          <div>
+            <div>{firstEvo.level ? firstEvo.level : null}</div>
+            <div>{firstEvo.item ? firstEvo.item : null}</div>
+          </div>
+          <div>
+            {evolutions[1] ? (
+              <>
+                <img src={evolutions[1].sprites.front_default} />
+              </>
+            ) : null}
+          </div>
+          <div>
+            <div>{secondEvo.level ? secondEvo.level : null}</div>
+            <div>{secondEvo.item ? secondEvo.item : null}</div>
+            <div>{secondEvo.trigger ? secondEvo.trigger : null}</div>
+          </div>
+          <div>
+            {evolutions[2] ? (
+              <img src={evolutions[2].sprites.front_default} />
+            ) : null}
+          </div>
         </>
-      ) : null}
+      ) : (
+        <Loading />
+      )}
     </>
   );
 }
